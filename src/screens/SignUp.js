da5be767a7c5255844ce68e6661894986437a7dc
Loussot <theo.loussot@gmail.com>
2022-12-06 12:16:03 +0100

    signUp

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -2,15 +2,24 @@ import React from "react";
 import "../styles/SignUp.css";
 
 export default function SignUp() {
+
+  const register = (e) => {
+    e.preventDefault();
+  }
+  const signIn = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="signUp">
       <form>
         <h1>S'identifier</h1>
         <input placeholder="Email ou numéro de téléphone" type="email"></input>
         <input placeholder="Mot de passe" type="password"></input>
-        <button type="submit">S'identifier</button>
+        <button type="submit" onClick={signIn}>S'identifier</button>
         <h4>
-          <span className="signUp_greyText">Première visite sur Netflix ?</span> Inscrivez-vous.
+          <span className="signUp_greyText">Première visite sur Netflix ?</span> 
+          <span className="signUp_link" onClick={register}>Inscrivez-vous.</span>
         </h4>
       </form>
     </div>
